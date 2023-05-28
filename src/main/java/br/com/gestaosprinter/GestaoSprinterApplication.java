0bbeba051a3113862d8b53c5893445e24771c6ed
Acelerabit <regio.pimentel2016@gmail.com>
2023-05-28 13:08:38 -0300

    conclui o context-path

--- src/main/java/br/com/gestaosprinter/GestaoSprinterApplication.java
+++ src/main/java/br/com/gestaosprinter/GestaoSprinterApplication.java
@@ -2,9 +2,18 @@ package br.com.gestaosprinter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GestaoSprinterApplication {
+	@GetMapping
+	public String gethomeTeste(){
+		return "sprinter - API HOME";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoSprinterApplication.class, args);
