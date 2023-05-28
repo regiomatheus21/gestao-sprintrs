0cf57fcaf4093c0d9f1f7b846aa77e596ed77e58
Acelerabit <regio.pimentel2016@gmail.com>
2023-05-28 12:34:31 -0300

    inicia o projeto

---
+++ src/main/java/br/com/gestaosprinter/GestaoSprinterApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaosprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSprinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSprinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaosprinter/GestaoSprinterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaosprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoSprinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
